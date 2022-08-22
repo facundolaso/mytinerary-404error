ca70f921dc4059e6b693954e0c327ad3bc076829
facundolaso <lasofacundo@gmail.com>
2022-08-22 16:11:13 -0300

    agregado de import CitiesPage

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import EventsCarousel from './components/EventsCarousel';
 import Welcome from './components/Welcome.js';
 import PageLayout from './layout/PageLayout';
+import CitiesPage from './pages/CitiesPage';
 import UnderConstruction from './pages/UnderConstruction.js';
 
 
 function App() {
   return (
     <div>
-    <PageLayout> 
+    {/* <PageLayout>  */}
+    <CitiesPage/>
     {/* <Welcome/> */}
     {/* <EventsCarousel/> */}
     {/* <UnderConstruction/> */}
-    </PageLayout>
+    {/* </PageLayout> */}
     </div>
   );
 }
