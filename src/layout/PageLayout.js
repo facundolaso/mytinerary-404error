fc104f585deee9d82d87a7841ea5f135db338cb6
facundolaso <lasofacundo@gmail.com>
2022-08-23 16:03:44 -0300

    agregado de header en layout page

--- src/layout/PageLayout.js
+++ src/layout/PageLayout.js
@@ -1,8 +1,11 @@
 import Footer from '../components/Footer' 
+import Header from '../components/Header'
+
 
 function PageLayout(props) {
     return (
         <div>
+            <Header/>
             { props.children }
             <Footer />
         </div>
