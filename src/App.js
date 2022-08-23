a77edcdd022f04f1b19a5b7e761ef77657d04838
engelberthRetamal <engel.retamal.e@gmail.com>
2022-08-23 15:19:33 -0400

    Se cambió Welcome por HomePage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import './App.css';
-import EventsCarousel from './components/EventsCarousel';
-import Header from './components/Header';
-import Welcome from './components/Welcome.js';
+import HomePage from './pages/HomePage';
 import PageLayout from './layout/PageLayout';
 import CitiesPage from './pages/CitiesPage';
 import NewCityPage from './pages/NewCityPage';
 import UnderConstruction from './pages/UnderConstruction.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-
-
 function App() {
   return (
-<BrowserRouter>
-  <PageLayout> 
-    <Routes>
-      <Route path='/' element={<Welcome/>} />
-      <Route path='/cities' element={<CitiesPage/>} />
-      <Route path='/new-city' element={<NewCityPage/>} />
-      <Route path='*' element={<UnderConstruction/> } />
-      </Routes>
-    </PageLayout>
-</BrowserRouter>
+    <BrowserRouter>
+      <PageLayout>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/cities' element={<CitiesPage />} />
+          <Route path='/new-city' element={<NewCityPage />} />
+          <Route path='*' element={<UnderConstruction />} />
+        </Routes>
+      </PageLayout>
+    </BrowserRouter>
 
   );
 }
