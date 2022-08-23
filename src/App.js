0c9a6239648a1b2d952aa564941316a6411e788d
facundolaso <lasofacundo@gmail.com>
2022-08-23 16:03:09 -0300

    agregado de rutas y browser-router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import './App.css';
 import EventsCarousel from './components/EventsCarousel';
+import Header from './components/Header';
 import Welcome from './components/Welcome.js';
 import PageLayout from './layout/PageLayout';
 import CitiesPage from './pages/CitiesPage';
 import NewCityPage from './pages/NewCityPage';
 import UnderConstruction from './pages/UnderConstruction.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 
 function App() {
   return (
-    <div>
-    <PageLayout> 
-    {/* <NewCityPage/> */}
-    <Welcome/>
-    <EventsCarousel/>
-    <UnderConstruction/>
+<BrowserRouter>
+  <PageLayout> 
+    <Routes>
+      <Route path='/' element={<Welcome/>} />
+      <Route path='/cities' element={<CitiesPage/>} />
+      <Route path='/new-city' element={<NewCityPage/>} />
+      <Route path='*' element={<UnderConstruction/> } />
+      </Routes>
     </PageLayout>
-    </div>
+</BrowserRouter>
+
   );
 }
 
