9ae2392864be450a65f1fc509430c8db3d1e60e7
engelberthRetamal <engel.retamal.e@gmail.com>
2022-08-25 18:14:57 -0400

    Se agrega el linkeo de cada ciudad hacia la PaginaNotFound

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/CityCard.css';
+import {Link as LinkRouter} from 'react-router-dom'
 
 export default function CityCard(props) {
 
@@ -8,12 +9,12 @@ export default function CityCard(props) {
 
     return (
 
-            <div className='card' style={{ backgroundImage:  `url(${dataCity.url})` }}>
-                <div className='info'>
-                    <h1 className='title'>{dataCity.title}</h1>
-                    <p className='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <button className="card-button">Read More</button>
-                </div>
+        <div className='card' style={{ backgroundImage: `url(${dataCity.url})` }}>
+            <div className='info'>
+                <h1 className='title'>{dataCity.title}</h1>
+                <p className='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <LinkRouter className="card-button" to={'*'}> Read More</LinkRouter>
             </div>
+        </div>
     )
 }
