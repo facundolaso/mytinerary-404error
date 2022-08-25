b3c0a11e6f4e5c8160aa5e1619b5ee97d30ea527
engelberthRetamal <engel.retamal.e@gmail.com>
2022-08-25 17:53:23 -0400

    creacion de template para card

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -3,17 +3,17 @@ import '../styles/CityCard.css';
 
 export default function CityCard(props) {
 
-    
+    const dataCity = props.data
+    console.log(dataCity.url)
 
     return (
-        <div className='City-container'>
-                <div class='card' style={{backgroundImage: props.url }}>
-                    <div class='info'>
-                        <h1 class='title'>HOLA</h1>
-                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius esse corporis, velit porro impedit laudantium accusamus! Id velit, illum magni rem mollitia blanditiis iste maiores optio ipsa, est dolorem fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius esse corporis, velit porro impedit laudantium accusamus! Id velit, illum magni rem mollitia blanditiis iste maiores optio ipsa, est dolorem fugit.</p>
-                        <button class="card-button">Read More</button>
-                    </div>
+
+            <div className='card' style={{ backgroundImage:  `url(${dataCity.url})` }}>
+                <div className='info'>
+                    <h1 className='title'>{dataCity.title}</h1>
+                    <p className='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <button className="card-button">Read More</button>
                 </div>
-        </div>
+            </div>
     )
 }
