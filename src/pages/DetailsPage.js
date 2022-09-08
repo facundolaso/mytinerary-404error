bab89f6775150453e8344f507cf6a9e70fce4a55
facundolaso <lasofacundo@gmail.com>
2022-09-07 23:57:20 -0300

    se agregaron formas de obtener datos desde la API

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import CityDetails from '../components/CityDetails'
-import ItineraryCard from '../components/ItineraryCard'
+import Itinerary from '../components/ItineraryCard'
 import '../styles/Details.css'
+import { useEffect } from 'react'
+import { useGetItineraryCityQuery, useGetDetailCityQuery} from '../features/dataAPI'
 
 export default function DetailsPage() {
+    let id
+    let queryString = window.location.search
+    let params = new URLSearchParams(queryString)
+    id = params.get("id")
+    let { data: itineraries } = useGetDetailCityQuery(id)
+    let { data: itinerariesCity } = useGetItineraryCityQuery(id)
+
   return (
     <div className='All-Container'>   
-      <CityDetails/>
-      <ItineraryCard/>
+      <CityDetails search={itineraries}/>
+      <Itinerary search={itinerariesCity}/>
     </div>
 
   )
