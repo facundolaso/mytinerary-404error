472d31c9be4e82ea5f71e2b42ce723417af0d3b8
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-14 12:36:14 -0300

    Se para como parametro un usuario logueado dentro del localStorage a el SignUp

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -2,7 +2,13 @@ import React from 'react'
 import SignUp from '../components/SignUp'
 
 export default function NewCityPage() {
+  let loggedUser = {}
+
+  if (localStorage.getItem("loggedUser")) {
+    loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
+  }
+
   return (
-        <SignUp/>
+        <SignUp loggedUser={loggedUser.user}/>
   )
 }
