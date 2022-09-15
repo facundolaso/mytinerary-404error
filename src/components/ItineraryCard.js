d3de13b169ac7bbd63060599569f475acd1430fe
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-14 23:55:57 -0300

    Se agregaron botones para editar y eliminar el itinerario en el que se encuentra

--- src/components/ItineraryCard.js
+++ src/components/ItineraryCard.js
@@ -2,10 +2,21 @@ import React from "react";
 import '../styles/ItineraryCard.css'
 import Activities from "./Activities";
 import Comments from "./Comments";
+import { useDeleteItineraryMutation } from "../features/itineraiesSlice";
 
 
-export default function Itinerary({search}) {
+export default function Itinerary({ search }) {
+
   let itineraries = search
+
+  const [ deleteCity , result ] = useDeleteItineraryMutation()
+
+  const handleAddTask = async () => {
+    let itineraryId = itineraries?.response[0]._id
+      await deleteCity(itineraryId);
+      console.log(itineraryId)
+      window.location.reload()
+  }
 
   const itineraryView = (itinerary) => (
 
@@ -13,7 +24,7 @@ export default function Itinerary({search}) {
       <div className="itinerary-card-body">
         <span className="itinerary-tag tag-teal">{itinerary.city.city}</span>
         <h4>
-          {itinerary.name} Itinerary 
+          {itinerary.name} Itinerary
         </h4>
         <p>
           Duration: {itinerary.duration}
@@ -25,19 +36,29 @@ export default function Itinerary({search}) {
           Tags: {itinerary.tags}
         </p>
         <div className="itinerary-user">
-          <img src={itinerary.user.photo} alt="user" />
           <div className="itinerary-user-info">
+            <img src={itinerary.user.photo} alt="user" />
+            <div>
             <h5>{itinerary.user.name}</h5>
             <small>Likes: {itinerary.likes}</small>
+            </div>
           </div>
+          {localStorage.getItem("loggedUser") ?
+              <div>
+                <button className='itineraryUser-button' type="">Modificar</button>
+                <button className='itineraryUser-button' onClick={handleAddTask}>Eliminar</button>
+              </div>
+              :
+              ''
+            }
         </div>
-        <Activities itinerary={itinerary._id}/>
-        <Comments itinerary={itinerary._id}/>
+        <Activities itinerary={itinerary._id} />
+        <Comments itinerary={itinerary._id} />
       </div>
     </div>
-    
-) 
-return (
+
+  )
+  return (
     <div className="itinerary-container">
       {itineraries?.response.map(itineraryView)}
     </div>
