ab2eeb42635e7baee14624f68641eb53a410ba59
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-21 13:59:37 -0300

    Se agregó funcion y componente de likes

--- src/components/ItineraryCard.js
+++ src/components/ItineraryCard.js
@@ -2,27 +2,39 @@ import React from "react";
 import '../styles/ItineraryCard.css'
 import Activities from "./Activities";
 import Comments from "./Comments";
-import { useDeleteItineraryMutation } from "../features/itineraiesSlice";
+import { useDeleteItineraryMutation, useLikeMutation } from "../features/itineraiesSlice";
 import Alerts from "./Alerts";
-
+import Like from "./Like";
 
 export default function Itinerary({ search, refetchAction }) {
 
   let itineraries = search
 
-
-  const [ deleteCity , result ] = useDeleteItineraryMutation()
+  const [deleteCity, result] = useDeleteItineraryMutation()
   const handleAddTask = async (itineraryId) => {
-      await deleteCity(itineraryId);
-      refetchAction()
+    await deleteCity(itineraryId);
+    refetchAction()
   }
 
   let loggedUser
   if (localStorage.getItem("loggedUser")) {
     loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
+  }
 
-}
+  const [like] = useLikeMutation()
+  async function handleLike(itinerary) {
+    if (localStorage.getItem('token')) {
+      try {
+        const it = await like(itinerary.target.id);
+        if (it.data?.success) {
+          refetchAction()
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
+  }
 
   const itineraryView = (itinerary) => (
 
@@ -46,26 +58,26 @@ export default function Itinerary({ search, refetchAction }) {
             <img src={itinerary.user.photo} alt="user" />
             <div>
               <h5>{itinerary.user.name}</h5>
-              <small>Likes: {itinerary.likes}</small>
+              <Like handleLike={handleLike} itinerary={itinerary}></Like>
+              <small>Likes: {itinerary.likes.length}</small>
             </div>
           </div>
           {localStorage.getItem("loggedUser") ?
             <>
-              {loggedUser.user.id == itinerary.user._id ? 
-              <div>
-                <button className='itineraryUser-button' type="">Modificar</button>
+              {loggedUser.user.id == itinerary.user._id ?
+                <div>
+                  <button className='itineraryUser-button' type="">Modificar</button>
 
-                <button className='itineraryUser-button' onClick={handleAddTask}>Eliminar</button>
-              </div> 
-              : 
-              <>
-              {loggedUser.user.role == "admin" ?               <div>
-              <button className='itineraryUser-button' type="">Modificar</button>
-            </div> : ''}
+                  <button className='itineraryUser-button' onClick={handleAddTask}>Eliminar</button>
+                </div>
+                :
+                <>
+                  {loggedUser.user.role == "admin" ? <div>
+                    <button className='itineraryUser-button' type="">Modificar</button>
+                  </div> : ''}
+                </>
+              }
             </>
-            }
-            </>
-
             :
             ''
           }
@@ -74,8 +86,8 @@ export default function Itinerary({ search, refetchAction }) {
         <Comments itinerary={itinerary._id} />
       </div>
     </div>
-
   )
+
   return (
     <div className="itinerary-container">
       {itineraries?.response.map(itineraryView)}
